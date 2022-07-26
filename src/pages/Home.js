00dab1522e225803ed4ad57e4cdc6f95742322fa
nitya2002 <nitya.malik936@gmail.com>
2022-07-26 09:33:08 +0530

    this is  second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,33 +4,54 @@ import Mainpage from '../Components/Mainpage'
 function Home  ()  {
 const [input, setInput]=useState('');
 const [results,setResults]=useState(null);
+const [searchOptions,setsearch]=useState('shows');
 const onChanget  =(ev)=>{
     // console.log(ev.target.value);
     setInput(ev.target.value)
 }
+const ischecked=searchOptions==="shows";
 const onSearch=()=>{
     // https://api.tvmaze.com/search/shows?q=girls
+    if(searchOptions==="shows"){
     fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r =>r.json()).then(result =>{
         setResults(result);
         console.log(result);
     })
+}
+    if(searchOptions==="person"){
+        fetch(`https://api.tvmaze.com/search/people?q=${input}`).then(r =>r.json()).then(result =>{
+        setResults(result);
+        console.log(result);
+    })
+}
 }
 const keyDown=(ev)=>{
     if(ev.keyCode===13){
         onSearch();
     }
 }
+const updatesearch=(ev)=>{
+    setsearch(ev.target.value);
+}
 function renderResults(){
     if(results && results.length===0){
         return <div>No Result found</div>
     }
     if(results && results.length>0)
-    return <div>{results.map(item=> (<div key={item.show.id}>{item.show.name} </div>))}</div>
+    return results[0].shows?results.map(item=> (<div key={item.shows.id}>{item.show.name} </div>)):results.map(item=> (<div key={item.person.id}>{item.person.name} </div>))
     return null;
 }
   return (
     <Mainpage>
-        <input type='text' onChange={onChanget} value={input} onKeyDown={keyDown}/>
+        <input type='text' onChange={onChanget} value={input} onKeyDown={keyDown}/><br/>
+        <label htmlFor='search-shows'>
+        Shows
+         <input id="search-shows" type="radio" value='shows' checked={ischecked} onChange={updatesearch}/>
+        </label>
+        <label htmlFor='search-actors'>
+            Actors
+            <input id='search-actors' type="radio" checked={!ischecked} value='person' onChange={updatesearch}/>
+        </label><br/>
         <button type="submit" onClick={onSearch}>Search</button>
         {renderResults()}
     </Mainpage>
